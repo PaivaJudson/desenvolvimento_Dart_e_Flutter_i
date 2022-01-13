ca51f2c2d3d6e2235285b6b6bf88cfed201bc197
Judson Paiva <judson.paiva@hotmail.com>
2022-01-13 03:00:42 +0100

    Herança e subescrita do método Fazer Som em Cachorro

--- heranca/exemplo1/Animal.dart
+++ heranca/exemplo1/Animal.dart
@@ -2,17 +2,12 @@
  * Class Animal 
  */
 
-
-
-/* 
- * Class Cachorro
- */
-
-class Cachorro{
+class Animal{
 
   late String nome;
   late double peso;
-  late int fofura;
+
+  Animal(this.nome, this.peso);
 
   void comer(){
     print("$nome comeu");
@@ -22,9 +17,27 @@ class Cachorro{
     print("Fez algum som");
   }
 
+}
+
+
+/* 
+ * Class Cachorro
+ */
+
+class Cachorro extends Animal{
+
+  late int fofura;
+
+  Cachorro(String nome, double peso, this.fofura) : super(nome, peso);
+  
   void brincar(){
     fofura += 10;
     print("Fofura do $nome aumentou para $fofura");
+  }
+
+  @override
+  void fazerSom(){
+    print("Au, au, au, au");
   }
 
 }
@@ -33,29 +46,29 @@ class Cachorro{
  * Class Gato 
  */
 
-class Gato{
+class Gato extends Animal{
 
-  late String nome;
-  late double peso;
-
-
-  void comer(){
-    print("$nome comeu!");
-  }
-
-  void fazerSom(){
-    print("$nome fez algum som!");
-  }
+  Gato(String nome, double peso) : super(nome, peso); 
 
   bool estaAmigavel() => true;
 
 }
-
 
 /* 
  * Função principal
  */
 
 void main(){
+
+  Cachorro c1 = Cachorro("Noque", 5.0, 5);
+  Gato g1 = Gato("Linda", 2.5);
+
+  c1.fazerSom();
+  c1.comer();
+  c1.brincar();
+
+  g1.fazerSom();
+  g1.comer();
+  print("Está amigável?  ${g1.estaAmigavel()}");
 
 }
